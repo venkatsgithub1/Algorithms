a86886a4c37bb76c0b0c4006119f12fa4e9a3a9c
Ven <venkatsgithub@gmail.com>
2018-12-12 07:59:29 +0530

    Added Kadane's algorithm to the repository.

---
+++ algos/KadanesAlgorithm.java
@@ -0,0 +1,21 @@
+package learning.algos;
+
+public class KadanesAlgorithm {
+    public static void main(String[] args) {
+        int a[] = new int[]{1, 2, 3};
+        int b[] = new int[]{-1, -2, -3, -4};
+
+        System.out.println(getKadaneSum(a));
+        System.out.println(getKadaneSum(b));
+    }
+
+    private static int getKadaneSum(int a[]) {
+        int globalMax = a[0], currentMax = a[0];
+        for (int i = 1; i < a.length; i++) {
+            currentMax = Integer.max(a[i], currentMax + a[i]);
+            globalMax = Integer.max(currentMax, globalMax);
+        }
+
+        return globalMax;
+    }
+}
